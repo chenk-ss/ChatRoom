479caab310afef12f610405546a4e724d6390b98
kakajy <1510687833@qq.com>
2020-03-08 21:31:45 +0800

    添加查看历史记录功能、显示在线人数

--- src/main/java/com/chenk/chatroom/controller/MessageController.java
+++ src/main/java/com/chenk/chatroom/controller/MessageController.java
@@ -1,6 +1,10 @@
 package com.chenk.chatroom.controller;
 
+import com.chenk.chatroom.pojo.bean.MessageBean;
+import com.chenk.chatroom.pojo.entity.Message;
 import com.chenk.chatroom.pojo.param.MessageParam;
+import com.chenk.chatroom.result.JsonResult;
+import com.chenk.chatroom.service.MessageService;
 import com.chenk.chatroom.socket.WebSocketServer;
 import com.google.gson.Gson;
 import lombok.extern.log4j.Log4j2;
@@ -10,7 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
 import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
 @Log4j2
 @Controller
@@ -18,19 +26,40 @@ public class MessageController {
 
     Gson GSON = new Gson();
 
+    @Resource
+    private MessageService messageService;
+
     @RequestMapping(value = "/")
-    public String index(Model m){
+    public String index(Model m) {
+//        List<Message> messages=messageService.query();
+//        m.addAttribute("messages",messages);
         return "Index";
     }
 
     @ResponseBody
+    @RequestMapping(value = "/query/history")
+    public JsonResult<List<Message>> history() {
+        JsonResult<List<Message>> result = new JsonResult<List<Message>>();
+        List<Message> messages = messageService.query();
+        result.setData(messages);
+        result.setSuccess(true);
+        return result;
+    }
+
+    @ResponseBody
     @RequestMapping(value = "/send")
-    public void sendMessage(@RequestParam("userName") String userName,@RequestParam("message") String message){
-        MessageParam param=new MessageParam();
-        param.setUserName(userName);
-        param.setMessage(message);
+    public void sendMessage(@RequestParam("userName") String userName, @RequestParam("message") String message) {
+//        MessageParam param = new MessageParam();
+//        param.setUserName(userName);
+//        param.setMessage(message);
+        MessageBean bean = new MessageBean();
+        bean.setId(UUID.randomUUID().toString());
+        bean.setCreateTime(new Date());
+        bean.setMessage(message);
+        bean.setUserName(userName);
+        messageService.add(bean);
         try {
-            WebSocketServer.sendInfo(GSON.toJson(param), "1");
+            WebSocketServer.sendInfo(GSON.toJson(bean), "1");
         } catch (IOException e) {
             log.error(e.getMessage());
         }
--- src/main/java/com/chenk/chatroom/socket/WebSocketServer.java
+++ src/main/java/com/chenk/chatroom/socket/WebSocketServer.java
@@ -26,19 +26,22 @@ public class WebSocketServer {
 
     //接收sid
     private String sid = "";
+
     /**
-     * 连接建立成功调用的方法*/
+     * 连接建立成功调用的方法
+     */
 
 
     @OnOpen
-    public void onOpen(Session session,@PathParam("sid") String sid) {
+    public void onOpen(Session session, @PathParam("sid") String sid) {
         this.session = session;
         webSocketSet.add(this);     //加入set中
         addOnlineCount();           //在线数加1
-        log.info("有新窗口开始监听:"+sid+",当前在线人数为" + getOnlineCount());
-        this.sid=sid;
+        log.info("有新窗口开始监听:" + sid + ",当前在线人数为" + getOnlineCount());
+        this.sid = sid;
         try {
             sendMessage("连接成功");
+            sendInfo("num:" + String.valueOf(onlineCount), sid);
         } catch (IOException e) {
             log.error("websocket IO异常");
         }
@@ -52,15 +55,21 @@ public class WebSocketServer {
         webSocketSet.remove(this);  //从set中删除
         subOnlineCount();           //在线数减1
         log.info("有一连接关闭！当前在线人数为" + getOnlineCount());
+        try {
+            sendInfo("num:" + String.valueOf(onlineCount), sid);
+        } catch (IOException e) {
+            log.error("websocket IO异常");
+        }
     }
 
     /**
      * 收到客户端消息后调用的方法
      *
-     * @param message 客户端发送过来的消息*/
+     * @param message 客户端发送过来的消息
+     */
     @OnMessage
     public void onMessage(String message, Session session) {
-        log.info("收到来自窗口"+sid+"的信息:"+message);
+        log.info("收到来自窗口" + sid + "的信息:" + message);
         //群发消息
         for (WebSocketServer item : webSocketSet) {
             try {
@@ -72,7 +81,6 @@ public class WebSocketServer {
     }
 
     /**
-     *
      * @param session
      * @param error
      */
@@ -81,6 +89,7 @@ public class WebSocketServer {
         log.error("发生错误");
         error.printStackTrace();
     }
+
     /**
      * 实现服务器主动推送
      */
@@ -91,15 +100,15 @@ public class WebSocketServer {
 
     /**
      * 群发自定义消息
-     * */
-    public static void sendInfo(String message,@PathParam("sid") String sid) throws IOException {
-        log.info("推送消息到窗口"+sid+"，推送内容:"+message);
+     */
+    public static void sendInfo(String message, @PathParam("sid") String sid) throws IOException {
+        log.info("推送消息到窗口" + sid + "，推送内容:" + message);
         for (WebSocketServer item : webSocketSet) {
             try {
                 //这里可以设定只推送给这个sid的，为null则全部推送
-                if(sid==null) {
+                if (sid == null) {
                     item.sendMessage(message);
-                }else if(item.sid.equals(sid)){
+                } else if (item.sid.equals(sid)) {
                     item.sendMessage(message);
                 }
             } catch (IOException e) {
